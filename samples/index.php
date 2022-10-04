df86c1002a81f2a8c3d6b3ef1141845d15f509e8
Rasheed Al-Wahbany <rasheedalwahbany@gmail.com>
2022-10-04 21:50:35 +0300

    Complete reading from database

--- samples/index.php
+++ samples/index.php
@@ -13,14 +13,22 @@ if($connection){
     $query = $connection->prepare("SELECT * FROM `cities`");
     if($query->execute()){
         $is_header = true;
+        $row_index = 1;
         while( $row = $query->fetchObject()){
-            $row_index = $sheet->getHighestRow()+1;
+        //     // $row_index = $sheet->getHighestRow()+1;
             $sheet->insertNewRowBefore($row_index);
-            
+            $i=0;
             foreach( $row as $key => $value ){
-                if($is_header)
-                $sheet->setCellValue($columns[$row_index].''.$row_index, $value);
+                if($is_header){
+                    $sheet->setCellValue($columns[$i].''.$row_index, $key);
+                }else{
+                    $sheet->setCellValue($columns[$i].''.$row_index, $value);
+                }
+                $i=$i+1;
             }
+            $is_header=false;
+            $row_index = $row_index + 1;
+
         }
     }
 }
